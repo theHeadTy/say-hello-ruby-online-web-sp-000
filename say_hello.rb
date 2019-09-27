885dedbb11ae5e4d99be702fc933007b8819e126
generic-puppet-7755 <tasmith1990@gmail.com>
2019-09-27 22:38:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 # Build your say_hello method here
 
-def say_hello(name = "Ruby Programmer")
+def say_hello(name = "Hello Ruby Programmer!")
   
-  puts "Hello {name}!"
+  puts name
   
 end
 
-say_hello("Hello Kent Beck")
+say_hello("Hello Kent Beck!")
